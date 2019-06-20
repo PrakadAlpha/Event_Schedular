889bfdf81df3c64c2baa13ed99168ab43a32ab79
PrakadAlpha <prakadheshwarancbe@gmail.com>
2019-06-20 07:07:53 +0530

    Update Event_Repo.java

--- Event_Schedular/src/main/java/com/ge/repository/Event_Repo.java
+++ Event_Schedular/src/main/java/com/ge/repository/Event_Repo.java
@@ -1,11 +1,19 @@
 package com.ge.repository;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ge.bean.Event;
 
+
 @Repository
 public interface Event_Repo extends CrudRepository<Event, Integer>{
 	
+	@Query("FROM Event e WHERE e.startDate between :sDate and :eDate")
+	List<Event> getByDate(@Param("sDate") Date sDate, @Param("eDate") Date eDate);
 }
