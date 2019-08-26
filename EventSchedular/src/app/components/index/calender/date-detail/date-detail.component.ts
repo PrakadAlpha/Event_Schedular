a3b445205ebec5364da6a7d2bc58b008135f2139
PrakadAlpha <prakadheshwarancbe@gmail.com>
2019-08-27 04:29:23 +0530

    Update date-detail.component.ts

--- EventSchedular/src/app/components/index/calender/date-detail/date-detail.component.ts
+++ EventSchedular/src/app/components/index/calender/date-detail/date-detail.component.ts
@@ -1,16 +1,11 @@
 import { Component, OnInit, Input, Inject } from '@angular/core';
-import * as moment from "moment";
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { CalenderComponent } from '../calender.component';
 import { EventsService } from 'src/app/services/events.service';
+import { CalendarEvent } from 'src/app/modals/CalendarEvent';
+import { EventI } from 'src/app/modals/EventsI';
+import * as _ from "lodash";
 
-
-
-export interface CalendarEvent {
-  mDate?: moment.Moment;
-  selected?: boolean;
-  today?: boolean;
-}
 
 @Component({
   selector: 'app-date-detail',
@@ -20,27 +15,30 @@ export interface CalendarEvent {
 export class DateDetailComponent implements OnInit {
 
   date: CalendarEvent;
+  events: EventI[] = [];
 
-  constructor(@Inject(MAT_DIALOG_DATA) data: CalendarEvent,     private dialogRef: MatDialogRef<CalenderComponent>,private service: EventsService) { 
+  constructor(@Inject(MAT_DIALOG_DATA) data: CalendarEvent, private dialogRef: MatDialogRef<CalenderComponent>,private service: EventsService) { 
 
     this.date = data
   }
 
   ngOnInit() {
     const date = this.date.mDate.format("YYYY-MM-DD");
-    this.service.getByDate(date).subscribe(data => {
+    this.service.getByDate(date).subscribe(dataList => {
 
-      console.log(data);
+      for (const i in dataList) {
+        if (dataList.hasOwnProperty(i)) {
+          const element = dataList[i];
+          this.events.push(element as EventI);
+          this.events = _.uniqWith(this.events, _.isEqual);
+        }
+      }
       
     })
   }
 
-
-
+  //Closing the dialog
   onClose() {
     this.dialogRef.close();
   }
-
-
-
 }
