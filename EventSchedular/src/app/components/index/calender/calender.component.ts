2a2840eb25edb6743eeebb8945568f91cbc4e9de
PrakadAlpha <prakadheshwarancbe@gmail.com>
2019-08-27 04:33:35 +0530

    Update calender.component.ts

--- EventSchedular/src/app/components/index/calender/calender.component.ts
+++ EventSchedular/src/app/components/index/calender/calender.component.ts
@@ -5,25 +5,12 @@ import { EventsService } from "src/app/services/events.service";
 import { DatePipe, Time } from "@angular/common";
 import { MatDialogConfig, MatDialog } from '@angular/material';
 import { DateDetailComponent } from './date-detail/date-detail.component';
+import { EventI } from 'src/app/modals/EventsI';
 
 export interface CalendarEvent {
   mDate?: moment.Moment;
   selected?: boolean;
   today?: boolean;
-}
-
-export interface EventI {
-  id?: number;
-  appName?: string;
-  environment?: string;
-  startDate?: Date;
-  endDate?: Date;
-  eventName?: string;
-  eventType?: string;
-  eventDetails?: string;
-  startTime?: Time;
-  endTime?: Time;
-  level?: string;
 }
 
 @Component({
@@ -32,17 +19,27 @@ export interface EventI {
   styleUrls: ["./calender.component.sass"]
 })
 export class CalenderComponent implements OnInit {
+
   currentDate = moment();
-  secondDate = moment(this.currentDate).add(1, "months");
-  thirdDate = moment(this.secondDate).add(1, "months");
+  nextDate = moment(this.currentDate).add(1, "months");
+  prevDate = moment(this.currentDate).subtract(1, "months");
+
   dayNames = ["S", "M", "T", "W", "T", "F", "S"];
+
   weeks: CalendarEvent[][] = [];
   sortedDates: CalendarEvent[] = [];
+
   events: EventI[] = [];
   event: EventI[] = [];
+
   dates: string[];
+
   spinner = false;
+
   passDate: CalendarEvent;
+
+  startDate: moment.Moment;
+  endDate: moment.Moment;
 
   @Input() dropList: string[];
   @Input() selectedDates: CalendarEvent[] = [];
@@ -58,15 +55,15 @@ export class CalenderComponent implements OnInit {
 
   prevMonth(): void {
     this.currentDate = moment(this.currentDate).subtract(1, "months");
-    this.secondDate = moment(this.currentDate).subtract(1, "months");
-    this.thirdDate = moment(this.secondDate).subtract(1, "months");
+    this.prevDate = moment(this.currentDate).subtract(1, "months");
+    this.nextDate = moment(this.currentDate).add(1, "months");
     this.generateCalender();
   }
 
   nextMonth(): void {
     this.currentDate = moment(this.currentDate).add(1, "months");
-    this.secondDate = moment(this.currentDate).add(1, "months");
-    this.thirdDate = moment(this.secondDate).add(1, "months");
+    this.prevDate = moment(this.currentDate).subtract(1, "months");
+    this.nextDate = moment(this.currentDate).add(1, "months");
     this.generateCalender();
   }
 
@@ -86,22 +83,13 @@ export class CalenderComponent implements OnInit {
     return moment().isSame(moment(date), "day");
   }
 
-  selectDate(date: CalendarEvent): void {
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "auto";
-    dialogConfig.data = date;
-    this.dialog.open(DateDetailComponent, dialogConfig);
-    
-  }
-
   isSelected(date: moment.Moment): boolean {
     return (
       _.findIndex(this.selectedDates, selectedDate => {
         return moment(date).isSame(selectedDate.mDate, "day");
       }) > -1
     );
+    
   }
 
   isSelectedMonth(date: moment.Moment): boolean {
@@ -114,26 +102,37 @@ export class CalenderComponent implements OnInit {
   generateCalender() {
     this.spinner = true;
     const dates = this.fillDates(this.currentDate);
-    const dateArr = dates.map(data => {
-      return data.mDate.format("YYYY-MM-DD");
-    });
-
     const weeks: CalendarEvent[][] = [];
+
     while (dates.length > 0) {
       weeks.push(dates.splice(0, 7));
     }
+
     this.weeks = weeks;
 
     const getData = async () => {
-      await this.mapValues(dateArr);
+      
+      await this.mapValues();
 
+      //Filters the dropped events and drops in the event array and particular css props are applied
       this.event = this.events.filter(i => {
         return this.dropList.includes(i.appName);
       }); 
+
       this.spinner = false;
     };
 
     getData();
+  }
+
+  //Date Click function
+  selectDate(date: CalendarEvent): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "auto";
+    dialogConfig.data = date;
+    this.dialog.open(DateDetailComponent, dialogConfig);
   }
 
   // mapValues(){
@@ -146,9 +145,11 @@ export class CalenderComponent implements OnInit {
 
   // }
 
-  mapValues(dates: string[]) {
-    dates.forEach(date => {
-      this.service.getByDate(date).subscribe(dataList => {
+
+  //Mapping the values as of the interface
+
+  mapValues() {
+      this.service.getByRange(this.startDate.format("YYYY-MM-DD"), this.endDate.format("YYYY-MM-DD")).subscribe(dataList => {
         if (dataList.length) {
           for (const i in dataList) {
             if (dataList.hasOwnProperty(i)) {
@@ -159,9 +160,10 @@ export class CalenderComponent implements OnInit {
           }
         }
       });
-    });
   }
 
+  //Filling the dates for the calender
+  
   fillDates(currentMoment: moment.Moment): CalendarEvent[] {
     const firstOfMonth = moment(currentMoment)
       .startOf("month")
@@ -170,7 +172,11 @@ export class CalenderComponent implements OnInit {
       .startOf("month")
       .subtract(firstOfMonth, "days");
     const start = firstDayOfGrid.date();
-
+    this.startDate = firstDayOfGrid;
+    this.endDate = moment(this.startDate).add(70, 'd');
+    console.log(this.startDate);
+    console.log(this.endDate);
+        
     return _.range(start, start + 70).map(
       (date: number): CalendarEvent => {
         const d1 = moment(firstDayOfGrid).date(date);
@@ -181,6 +187,10 @@ export class CalenderComponent implements OnInit {
         };
       }
     );
+  }
+
+  print(){
+    window.print();
   }
 
 }
